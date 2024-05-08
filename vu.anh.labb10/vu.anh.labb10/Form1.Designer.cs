d2d45ece982527e230b630d5d877dd7a685d277e
atognoo_b <hoanganhvu100402@gmail.com>
2024-05-08 16:56:31 +0700

    Done

---
+++ vu.anh.labb10/vu.anh.labb10/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace vu.anh.labb10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmn_points = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmn_win = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmn_draw = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmn_lose = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(471, 325);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.clmn_points,
+            this.clmn_win,
+            this.clmn_draw,
+            this.clmn_lose});
+            this.dataGridView2.Location = new System.Drawing.Point(489, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView2.Size = new System.Drawing.Size(425, 325);
+            this.dataGridView2.TabIndex = 105;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Team";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // clmn_points
+            // 
+            this.clmn_points.HeaderText = "Points";
+            this.clmn_points.MinimumWidth = 6;
+            this.clmn_points.Name = "clmn_points";
+            this.clmn_points.ReadOnly = true;
+            // 
+            // clmn_win
+            // 
+            this.clmn_win.HeaderText = "Win";
+            this.clmn_win.MinimumWidth = 6;
+            this.clmn_win.Name = "clmn_win";
+            this.clmn_win.ReadOnly = true;
+            // 
+            // clmn_draw
+            // 
+            this.clmn_draw.HeaderText = "Draw";
+            this.clmn_draw.MinimumWidth = 6;
+            this.clmn_draw.Name = "clmn_draw";
+            this.clmn_draw.ReadOnly = true;
+            // 
+            // clmn_lose
+            // 
+            this.clmn_lose.HeaderText = "Lose";
+            this.clmn_lose.MinimumWidth = 6;
+            this.clmn_lose.Name = "clmn_lose";
+            this.clmn_lose.ReadOnly = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(404, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 39);
+            this.button3.TabIndex = 106;
+            this.button3.Text = "Next Round";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 454);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmn_points;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmn_win;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmn_draw;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmn_lose;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ vu.anh.labb10/vu.anh.labb10/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vu.anh.labb10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] teams = new string[8] { "ManU", "ManC", "Liverpool", "Chelsea", "Arsenal", "Tottenham", "Everton", "Fullham" };
+        int[] teams_id = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 };
+        float[] team_lamda = new float[8];
+        int[,] team_matches = new int[8, 8];
+        int rounds = 1;
+
+        Dictionary<string, Dictionary<string, int>> team_stat_name = new Dictionary<string, Dictionary<string, int>>();
+        private void main_diagonale_setter()
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                    if (i == j) team_matches[i, j] = 1;
+        }
+        private int match(double lamda)
+        {
+            int x = 0;
+            double sum = 0;
+            Random rnd = new Random();
+            while (sum > -lamda)
+            {
+                sum += Math.Log(rnd.NextDouble());
+                x++;
+            }
+            x -= 1;
+            return x;
+        }
+        private int[] teams_next_round_matches()
+        {
+            int[] teams_id_next = new int[8];
+            int k = 0;
+            int[] teams_temp = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i<8; i++)
+            {
+                if (teams_temp[i] ==0)
+                    for(int j= 0;j<8; j++)
+                    {
+                        if (team_matches[i, j] !=1 && teams_temp[j] == 0)
+                        {
+                            teams_id_next[k] = i;
+                            teams_id_next[k+1] = j;
+                            k += 2;
+                            team_matches[i, j] = 1;
+                            teams_temp[i] = 1;
+                            teams_temp[j] = 1;
+                            break;
+                        }
+                    }
+            }
+            return teams_id_next;
+        }
+        private void sort()
+        {
+            team_stat_name = team_stat_name.OrderByDescending(pair => pair.Value["point"]).ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+            button1.Visible = false;
+            button3.Visible = true;
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+            main_diagonale_setter();
+            Random rnd = new Random();
+            for (int i =0;i< teams.Length; i++)
+            {
+                team_lamda[i] = (float)rnd.Next(501) / 100;
+            }
+            for (int i = 0; i < teams.Length; i++)
+            {
+                dataGridView1.Columns.Add(new DataGridViewColumn { HeaderText = teams[i], CellTemplate = new DataGridViewTextBoxCell() });
+            }
+            for (int i = 0;i < teams.Length; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].HeaderCell = new DataGridViewRowHeaderCell { Value = teams[i] };
+            }
+            for (int i = 0; i < teams.Length; i++)
+                for (int j = 0; j < teams.Length; j++)
+                    if (i == j) dataGridView1[i, j].Value = "---";
+            for (int i = 0; i < teams.Length; i++)
+            {
+                team_stat_name[teams[i]] = new Dictionary<string, int>()
+                {
+                    ["point"] = 0,
+                    ["win"] = 0,
+                    ["draw"] = 0,
+                    ["lose"] = 0
+                };
+            }
+            foreach (var team in team_stat_name)
+            {
+                dataGridView2.Rows.Add(team.Key, team.Value["point"], team.Value["win"], team.Value["draw"], team.Value["lose"]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+            for (int i = 0; i < teams.Length; i++)
+                for (int j = 0; j < teams.Length; j++)
+                    dataGridView1[i, j].Style.BackColor = Color.White;
+            Random rnd = new Random();
+            int[] next_round = new int[8];
+            next_round = teams_next_round_matches();
+            for (int i = 0; i < teams.Length; i += 2)
+            {
+                int a = next_round[i];
+                int b = next_round[i + 1];
+                int goals_a = match(team_lamda[a]);
+                int goals_b = match(team_lamda[b]);
+
+                if (goals_a == goals_b)
+                {
+                    team_stat_name[teams[a]]["point"] += 1;
+                    team_stat_name[teams[a]]["draw"] += 1;
+                    team_stat_name[teams[b]]["point"] += 1;
+                    team_stat_name[teams[b]]["draw"] += 1;
+                }
+                else if (goals_a > goals_b)
+                {
+                    team_stat_name[teams[a]]["point"] += 3;
+                    team_stat_name[teams[a]]["win"] += 1;
+                    team_stat_name[teams[b]]["lose"] += 1;
+                }
+                else
+                {
+                    team_stat_name[teams[a]]["lose"] += 1;
+                    team_stat_name[teams[b]]["point"] += 3;
+                    team_stat_name[teams[b]]["win"] += 1;
+                }
+
+                dataGridView1[b, a].Value = $"{goals_a} - {goals_b}";
+                dataGridView1[b, a].Style.BackColor = Color.Green;
+            }
+            sort();
+            dataGridView2.Rows.Clear();
+            foreach (var team in team_stat_name)
+            {
+                dataGridView2.Rows.Add(team.Key, team.Value["point"], team.Value["win"], team.Value["draw"], team.Value["lose"]);
+            }
+
+            if (rounds == 14)
+            {
+                
+                dataGridView2.Rows[7].DefaultCellStyle.BackColor = Color.Gold;
+                dataGridView2.Rows[6].DefaultCellStyle.BackColor = Color.Silver;
+                dataGridView2.Rows[5].DefaultCellStyle.BackColor = Color.Brown;
+            }
+            rounds += 1;
+            dataGridView2.ClearSelection();
+            dataGridView2.ClearSelection();
+            dataGridView2.ClearSelection();
+            if (rounds == 15)
+            {
+                button1.Visible = false;
+                button3.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
